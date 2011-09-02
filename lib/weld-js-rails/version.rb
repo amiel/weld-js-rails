813f14ca9234da9ff3569b86e8c391fe4d6c07b7
Amiel Martin <amiel@carnesmedia.com>
2011-09-02 16:01:40 -0700

    Version 0.0.2 includes the optional jquery plugin

--- lib/weld-js-rails/version.rb
+++ lib/weld-js-rails/version.rb
@@ -1,7 +1,7 @@
 module Weld
   module Js
     module Rails
-      VERSION = "0.0.1"
+      VERSION = "0.0.2"
       WELD_VERSION = "0.2.1"
       WELD_REVISION = "76b9821bd7e2a2a7d733e947ba1838ebcc090755"
     end
